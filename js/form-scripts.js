6ecb33242ab66949cb8c027104dd69f15a127975
KozyrevaMariia <mariiakozyreva@gmail.com>
2017-12-23 06:07:05 +0300

    Хооол

--- js/form-scripts.js
+++ js/form-scripts.js
@@ -1,57 +1,19 @@
-$("#contactForm").validator().on("submit", function (event) {
-    if (event.isDefaultPrevented()) {
-        // handle the invalid form...
-        formError();
-        submitMSG(false, "Did you fill in the form properly?");
-    } else {
-        // everything looks good!
-        event.preventDefault();
-        submitForm();
-    }
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
+
+	// Change to your service ID, or keep using the default service
+	var service_id = "default_service";
+	var template_id = "newtemplate_forevstakhiy";
+
+	myform.find("button").text("Отправляется...");
+	emailjs.sendForm(service_id,template_id,"myform")
+		.then(function(){
+			alert("Отправлено!");
+			 myform.find("button").text("Отправить");
+		}, function(err) {
+			 alert("Не удалось отправить\r\n Response:\n " + JSON.stringify(err));
+			 myform.find("button").text("Отправить");
+		});
+	return false;
 });
-
-
-function submitForm(){
-    // Initiate Variables With Form Content
-
-
-
-
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
-
-    $.ajax({
-        type: "POST",
-        url: "contact.php",
-        data: "name=" + name + "&email=" + email + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            } else {
-                formError();
-                submitMSG(false,text);
-            }
-        }
-    });
-}
-
-function formSuccess(){
-    $("#contactForm")[0].reset();
-    submitMSG(true, "Message Submitted!")
-}
-
-function formError(){
-    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-        $(this).removeClass();
-    });
-}
-
-function submitMSG(valid, msg){
-    if(valid){
-        var msgClasses = "h3 text-center tada animated text-success";
-    } else {
-        var msgClasses = "h3 text-center text-danger";
-    }
-    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-}
