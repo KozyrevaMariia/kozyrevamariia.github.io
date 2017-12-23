325ce02b47b64cd04db51794d0de490092e630b7
KozyrevaMariia <mariiakozyreva@gmail.com>
2017-12-23 05:52:48 +0300

    Депрессия

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
 
-	var myform = $("form#myform");
-	myform.submit(function(event){
-		event.preventDefault();
+	// Change to your service ID, or keep using the default service
+	var service_id = "default_service";
+	var template_id = "newtemplate_forevstakhiy";
 
-	  // Change to your service ID, or keep using the default service
-	  var service_id = "default_service";
-	  var template_id = "newtemplate_forevstakhiy";
-
-	  myform.find("button").text("Отправляется...");
-	  emailjs.sendForm(service_id,template_id,"myform")
-	  	.then(function(){
-	    	alert("Отправлено!");
-	       myform.find("button").text("Отправить");
-	    }, function(err) {
-	       alert("Не удалось отправить\r\n Response:\n " + JSON.stringify(err));
-	       myform.find("button").text("Отправить");
-	    });
-	  return false;
-	});
+	myform.find("button").text("Отправляется...");
+	emailjs.sendForm(service_id,template_id,"myform")
+		.then(function(){
+			alert("Отправлено!");
+			 myform.find("button").text("Отправить");
+		}, function(err) {
+			 alert("Не удалось отправить\r\n Response:\n " + JSON.stringify(err));
+			 myform.find("button").text("Отправить");
+		});
+	return false;
+});
